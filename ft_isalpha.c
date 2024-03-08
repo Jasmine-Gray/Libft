fcba901ae1aea4fb2e0d778400e54f4cc89f84a0
Jasmine-Gray <145887933+Jasmine-Gray@users.noreply.github.com>
2024-03-08 23:37:53 +0900

    [feat] complete ft_isalpha.c

---
+++ ft_isalpha.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int ft_isupper(int c){
+	if ('A' <= c && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+int ft_islower(int c){
+	if ('a' <= c && c <= 'z')
+		return (1);
+	return (0);
+}
+
+int ft_isalpha(int c) 
+{
+	return (ft_isupper(c) || ft_islower(c));
+}
+
+// bool check_isalpha(void) 
+// {
+// 	int i = 0;
+// 
+// 	while (i < 1024) 
+// 	{
+// 		if (isalpha(i) != ft_isalpha(i)) 
+// 		{
+// 			printf("%d\t isalpha = %d, ft_isalpha = %d\n", i, isalpha(i), ft_isalpha(i));
+// 			return (false);
+// 		}
+// 		i++;
+// 	}
+// 	return (true);
+// }
+// 
+// int main(void) 
+// {
+// 	
+// 	if (check_isalpha()) 
+// 	{
+// 		printf("ok\n");
+// 	} else
+// 	{
+// 		printf("error\n");
+// 	}
+// }
